b7090e1a0ed792a754bd1f6887e4a552eea9996b
Emmanuel Cajas <rodri_go4@hotmail.com>
2022-02-11 17:59:47 -0500

    proyecto con las funcionalidades terminadas, solo falta adornarlo

---
+++ ahorcado.js
@@ -0,0 +1,143 @@
+
+//array de paises
+var palabras = ["CHILE", "BRASIL", "COLOMBIA", "PERU", "BOLIVIA", "ECUADOR"];
+
+
+var buttonIniciarJuego = document.querySelector("#iniciar-juego");
+buttonIniciarJuego.onclick = jugar;
+
+
+var error = 0;
+var i = 1;
+
+function jugar() {
+
+    cavasInicio();
+
+    var palabraEscogida = palabras[Math.floor(Math.random() * palabras.length)];
+    console.log(palabraEscogida);
+    
+    var palabraEscogidaSplit = palabraEscogida.split("");
+    console.log(palabraEscogidaSplit);
+
+    //Dibujar lineas para letras  de palabra escogida
+    var x = 700;
+    var y = 710;
+    const arrayEjeXaleatorio = [];
+
+    for (let i = 0; i < palabraEscogidaSplit.length; i++) {
+        pincel.fillStyle = "black";
+        pincel.fillRect(x, 600, 40, 3);
+        x += 60;
+        arrayEjeXaleatorio.push(y);
+        y += 60;
+    }
+
+    document.addEventListener("keypress", letraAdivinada);
+
+
+    function letraAdivinada() {
+        var letraPresionada = event.key;
+    
+        console.log(error);
+        console.log(letraPresionada);
+        var xy = "";
+        var posicion=-1;
+
+        for(var j=0;j<palabraEscogidaSplit.length;j++){
+
+            if(letraPresionada==palabraEscogidaSplit[j]){
+                pincel.beginPath();
+                pincel.fillStyle = "black";
+                pincel.font = "bold 50px arial";
+                pincel.fillText(palabraEscogidaSplit[j], arrayEjeXaleatorio[j], 570); 
+                posicion++;
+            }
+       
+        }
+
+        if (error >= 9) {
+            alert("perdiste");
+            i = 0;
+            error = 0;
+            posicion=-1;
+            location.reload(true)
+          
+        } else if (i == palabraEscogidaSplit.length) {
+            alert("G A N A S T E");
+            i = 0;
+            error = 0;
+            posicion=-1;
+            location.reload(true)
+        }
+    
+        if (posicion == -1) {
+            error++;
+            switch (error) {
+    
+                case 1:
+                    posteVertical();
+                    break;
+                case 2:
+                    posteHorizontal();
+                    break;
+                case 3:
+                    soga();
+                    break;
+                case 4:
+                    cabeza();
+                    break;
+                case 5:
+                    cuerpo();
+                    break;
+                case 6:
+                    brazoDerecho();
+                    break;
+                case 7:
+                    brazoIzquierdo();
+                    break;
+                case 8:
+                    piernaDerecha();
+                    break;
+                case 9:
+                    piernaIzquierda();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            i++;
+        }
+    
+    }
+ 
+}
+
+//ingresando palabra nueva al array
+
+var buttonNuevaPalabra = document.querySelector("#nueva-palabra");
+buttonNuevaPalabra.onclick = nuevaPalabra;
+
+function nuevaPalabra() {
+
+    var inputNuevaPalabra = document.querySelector("#input-nueva-palabra");
+    var nuevaPalabraAgregada = inputNuevaPalabra.value.toUpperCase();
+
+
+    if (palabras.includes(nuevaPalabraAgregada)) {
+        alert("La palabra " + nuevaPalabraAgregada + " ya existe... ingrese otra palabra")
+    } else {
+        alert("La palabra " + nuevaPalabraAgregada + " a sido agregada con exito...")
+        palabras.push(nuevaPalabraAgregada);
+        console.log(palabras)
+    }
+    inputNuevaPalabra.focus();
+}
+
+
+
+
+
+
+
+
--- canvas.js
+++ canvas.js
@@ -1,27 +1,30 @@
 
 var pantalla = document.querySelector("#ahorcado");
 var pincel = pantalla.getContext("2d");
-pincel.fillStyle = "lightgray";
-pincel.fillRect(0, 0, 1200, 800);
 
-//triangulo
-pincel.fillStyle = "black"; //propiedad
-pincel.beginPath();
-pincel.moveTo(200, 710);
-pincel.lineTo(100, 750);
-pincel.lineTo(300, 750);
-pincel.fill();
 
-pincel.fillStyle = "white"; //propiedad
-pincel.beginPath();
-pincel.moveTo(200, 715);
-pincel.lineTo(125, 745);
-pincel.lineTo(275, 745);
-pincel.fill();
+function cavasInicio() {
+
+    pincel.fillStyle = "lightgray";
+    pincel.fillRect(0, 0, 1200, 800);
+
+    //triangulo
+    pincel.fillStyle = "black"; //propiedad
+    pincel.beginPath();
+    pincel.moveTo(200, 710);
+    pincel.lineTo(100, 750);
+    pincel.lineTo(300, 750);
+    pincel.fill();
+
+    pincel.fillStyle = "white"; //propiedad
+    pincel.beginPath();
+    pincel.moveTo(200, 715);
+    pincel.lineTo(125, 745);
+    pincel.lineTo(275, 745);
+    pincel.fill();
+}
 
 //poste vertical
-
-
 function posteVertical() {
     pincel.fillStyle = "black";
     pincel.fillRect(197, 200, 6, 512);
@@ -91,113 +94,3 @@ function piernaIzquierda() {
     pincel.lineTo(400, 570);
     pincel.stroke();
 }
-
-//array de palabras
-var palabras = ["CHILE", "BRASIL", "COLOMBIA", "PERU", "BOLIVIA", "ECUADOR"];
-
-var palabraEscogida = palabras[Math.floor(Math.random() * palabras.length)];
-console.log(palabraEscogida);
-
-//Dibujar lineas para letras  de palabra escogida
-var x = 700;
-var y = 710;
-const arrayEjeXaleatorio = [];
-
-for (let i = 0; i < palabraEscogida.length; i++) {
-    pincel.fillStyle = "black";
-    pincel.fillRect(x, 600, 40, 3);
-    x += 60;
-    arrayEjeXaleatorio.push(y);
-    y += 60;
-}
-
-var indice = Math.floor(Math.random() * palabraEscogida.length)
-
-/*
-//Poner una letra ramdom de la palabraEscogida en la pantalla para que usuario tenga una pista de la palabra
-var letraEscogida = palabraEscogida[indice];
-var lugarParaLetraEscogida = arrayEjeXaleatorio[indice];
-
-console.log(letraEscogida);
-console.log(arrayEjeXaleatorio);
-
-
-pincel.beginPath();
-pincel.fillStyle = "black";
-pincel.font = "bold 50px arial";
-pincel.fillText(letraEscogida, lugarParaLetraEscogida, 570);
-*/
-document.addEventListener("keypress", letraAdivinada);
-
-var error = 0;
-var i = 0;
-var palabraEscogidaSplit=palabraEscogida.split("");
-console.log(palabraEscogidaSplit);
-function letraAdivinada() {
-    var letraPresionada = event.key;
-    
-    console.log(error);
-    console.log(letraPresionada);
-    var xy="";
-    xy=palabraEscogidaSplit.find(element => element==letraPresionada)
-    var posicion=palabraEscogidaSplit.indexOf(xy);
-   
-    pincel.beginPath();
-            pincel.fillStyle = "black";
-            pincel.font = "bold 50px arial";
-            pincel.fillText(xy, arrayEjeXaleatorio[posicion], 570);
-
-    if(error>=9){
-        alert("perdiste");
-        i=0;
-        error=0;
-    }else if(i==palabraEscogidaSplit.length){
-        alert("G A N A S T E");
-        i=0;
-        error=0;
-    }
-
-    console.log(xy);
-    console.log(posicion);
-    console.log(i);
-    
-    if(posicion==-1){
-        error++;
-        switch (error) {
-
-            case 1:
-                posteVertical();
-                break;
-            case 2:
-                posteHorizontal();
-                break;
-            case 3:
-                soga();
-                break;
-            case 4:
-                cabeza();
-                break;
-            case 5:
-                cuerpo();
-                break;
-            case 6:
-                brazoDerecho();
-                break;
-            case 7:
-                brazoIzquierdo();
-                break;
-            case 8:
-                piernaDerecha();
-                break;
-            case 9:
-                piernaIzquierda();
-                break;
-            default:
-                break;
-        }
-    }else{
-        i++;
-    }
-
-}
-
